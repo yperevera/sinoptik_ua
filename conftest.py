2e0bfc1d186ff421cb8bcc20c539b8f13c319f17
yperevera <y.perevera@gmail.com>
2018-01-12 13:55:54 +0200

    modified conftest.py; added first test of main page elements presenting; create tests runner for  main page;

--- conftest.py
+++ conftest.py
@@ -1,36 +1,24 @@
-import pytest
-from selenium.webdriver import Chrome 
+import pytets
+from selenium.webdriver import Chrome
 
 def pytest_addoption(parser):
-   parser.addoption("--driver", action="store", default="chrome", help="Type in browser type")
-   parser.addoption("--url", action="store", default="https://sinoptik.ua/", help="main url")
-   #parser.addoption("--username", action="store", default="manager", help="username")
-   #parser.addoption("--password", action="store", default="test", help="password")
-
-
-@pytest.fixture(scope="module", autouse=True)
-def driver(request):
-   browser = request.config.getoption("--driver")
-   if browser == 'chrome':
-       browser = Chrome()
-       browser.get("about:blank")
-       browser.implicitly_wait(10)
-       browser.maximize_window()
-       return browser
-   else:
-       print("Only chrome is supported at the moment.")
+   parser.addoption("--driver", action="store", default="chrome", help="Type in browser type.")
+   parser.addoption("--url", action="store", default="https://sinoptik.ua/", help="Type main url.")
 
 
 @pytest.fixture(scope="module")
-def url(request):
+def driver(request):
+   driver = request.config.getoption("--driver")
+   if driver == "chrome":
+      driver = Chrome()
+      driver.get("about:blank")
+      driver.implicitly_wait(10)
+      driver.maximize_window()
+      return driver
+   else:
+      print("Only Chrome browser is supported at the moment.")
+
+
+@pytest.fixture(scope="module")
+def main_url(request):
    return request.config.getoption("--url")
-
-
-#@pytest.fixture(scope="module")
-#def username(request):
-   #return request.config.getoption("--username")
-
-
-#@pytest.fixture(scope="module")
-#def password(request):
-   #return request.config.getoption("--password")
---
+++ tests/tests.py
@@ -0,0 +1,56 @@
+from selenium.webdriver.common.by import By
+
+# elements
+top_logo_xpath = "//div[@class='bLogo']/img"
+search_form_xpath = "//form[@id='form-search']"
+localization_xpath = "//div[@id='sLang']"
+city_name_xpath = "//div[@class='cityName cityNameShort']"
+top_menu_xpath = "//div[@id='topMenu']"
+main_content_block_xpath = "//div[@id='mainContentBlock']"
+bottom_ad_block_xpath = "//div[@id='leftCol']/div[contains(@style, 'width')]"
+other_cities_weather_xpath = "//div[@id='tenOtherCities']"
+right_ad_block_xpath = "//div[@id='rightCol']"
+footer_xpath = "//div[@id='footer']"
+copyright_xpath = "//div[@id='copyright']"
+
+# expected
+
+
+
+
+def get_elements_list_xpath(driver, xpath):
+    return driver.find_elements_by_xpath(xpath)
+
+
+def get_element_xpath(driver, xpath):
+    return driver.find_element_by_xpath(xpath)
+
+
+def open_url(driver, main_url):
+    driver.get(main_url)
+    assert driver.current_url == main_url
+
+
+def verify_main_page_elements_presence(driver):
+    # logo
+    assert get_element_xpath(top_logo_xpath).is_displayed() == True
+    # search form
+    assert get_element_xpath(search_form_xpath).is_displayed() == True
+    # localization
+    assert get_element_xpath(localization_xpath).is_displayed() == True
+    # city name
+    assert get_element_xpath(city_name_xpath).is_displayed() == True
+    # top menu
+    assert get_element_xpath(top_menu_xpath).is_displayed() == True
+    # main content block
+    assert get_element_xpath(main_content_block_xpath).is_displayed() == True
+    # bottom ads block
+    assert get_element_xpath(bottom_ad_block_xpath).is_displayed() == True
+    # other cities weather
+    assert get_element_xpath(other_cities_weather_xpath).is_displayed() == True
+    # right ad block
+    assert get_element_xpath(right_ad_block_xpath).is_displayed() == True
+    # footer
+    assert get_element_xpath(footer_xpath).is_displayed() == True
+    # copyright
+    assert get_element_xpath(copyright_xpath).is_displayed() == True
