9996c79c560f98e4b0cba38a38832ec65463ca1c
yperevera <y.perevera@gmail.com>
2018-01-12 15:59:45 +0200

    added one more test (3). ALL tests passed.

--- test_main_page.py
+++ test_main_page.py
@@ -7,3 +7,7 @@ def test_open_main_page(driver, main_url):
 # Test 2 Verify that ALL main elements are displayed
 def test_main_page_elements_presence(driver):
     tests.verify_main_page_elements_presence(driver)
+
+# Test 3 Verify logo attributes
+def test_top_logo_attributes(driver):
+    tests.varify_logo_attributes(driver)
--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
 
 # elements
-top_logo_xpath = "//div[@class='bLogo']/img"
+## logo
+top_logo_img_xpath = "//div[@class='bLogo']/img"
+top_logo_a_xpath = "//div[@class='bLogo']/img/../a"
+
 search_form_xpath = "//form[@id='form-search']"
 localization_xpath = "//div[@id='sLang']"
 city_name_xpath = "//div[@class='cityName cityNameShort']"
@@ -14,7 +17,13 @@ footer_xpath = "//div[@id='footer']"
 copyright_xpath = "//div[@id='copyright']"
 
 # expected
-
+## logo
+exp_top_logo_img_src = "https://sinst.fwdcdn.com/img/newImg/sinoptic-logo-ny-2018.png"
+exp_top_logo_img_alt = "погода"
+exp_top_logo_img_title = exp_top_logo_img_alt
+exp_top_logo_a_href = "https://sinoptik.ua/"
+exp_top_logo_a_title = exp_top_logo_img_alt
+exp_top_logo_text = "Прогноз погоды"
 
 
 
@@ -33,7 +42,7 @@ def open_url(driver, main_url):
 
 def verify_main_page_elements_presence(driver):
     # logo
-    assert get_element_xpath(driver, top_logo_xpath).is_displayed() == True
+    assert get_element_xpath(driver, top_logo_img_xpath).is_displayed() == True
     # search form
     assert get_element_xpath(driver, search_form_xpath).is_displayed() == True
     # localization
@@ -54,3 +63,22 @@ def verify_main_page_elements_presence(driver):
     assert get_element_xpath(driver, footer_xpath).is_displayed() == True
     # copyright
     assert get_element_xpath(driver, copyright_xpath).is_displayed() == True
+
+
+def varify_logo_attributes(driver):
+    img_element = get_element_xpath(driver, top_logo_img_xpath)
+    a_element = get_element_xpath(driver, top_logo_a_xpath)
+
+    # img src
+    assert img_element.get_attribute('src') == exp_top_logo_img_src
+    # img alt
+    assert img_element.get_attribute('alt') == exp_top_logo_img_alt
+    # img title
+    assert img_element.get_attribute('title') == exp_top_logo_img_title
+
+    # a href
+    assert a_element.get_attribute('href') == exp_top_logo_a_href
+    # a title
+    assert a_element.get_attribute('title') == exp_top_logo_a_title
+    # a text
+    assert a_element.text == exp_top_logo_text
